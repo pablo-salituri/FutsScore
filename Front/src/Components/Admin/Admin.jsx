8d1bbf734c132689d9ad0b622efb2d2c9356a179
Pablo Salituri <pablo_salituri@live.com.ar>
2024-01-24 14:12:59 -0300

    arquitectura de admin

--- Front/src/Components/Admin/Admin.jsx
+++ Front/src/Components/Admin/Admin.jsx
@@ -1,50 +1,13 @@
-import { useState } from "react";
-import { storage, firebaseApp } from "../Firebase/credentials";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
-import { v4 } from "uuid";
+import NavBar from "../NavBar/NavBar";
+import Home from "../Home/Home";
+import styles from "./Admin.module.css";
 
 export default function Admin() {
-  const firestore = getFirestore(firebaseApp);
-
-  const [imageUpload, setImageUpload] = useState(null);
-
-  async function createInFirestore(uid, url) {
-    const docuRef = doc(firestore, `Cards/${uid}`);
-
-    setDoc(docuRef, {
-      Description: "uploaded",
-      ImgUrl: url,
-      Price: 1278,
-    });
-  }
-
-  function uploadImage() {
-    if (imageUpload === null) return;
-    const name = v4();
-    const imageRef = ref(storage, `images/${name}`);
-    uploadBytes(imageRef, imageUpload).then(() => {
-      getDownloadURL(imageRef)
-        .then((url) => {
-          createInFirestore(name, url).then(() => alert("Image uploaded"));
-        })
-        .catch((error) => {
-          // Manejar errores si es necesario
-          console.error("Error al obtener la URL de descarga:", error);
-        });
-    });
-  }
-
   return (
-    <div>
-      Hola
-      <input
-        type="file"
-        onChange={(event) => {
-          setImageUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadImage}>Upload</button>
+    <div className={styles.adminContainer}>
+      <NavBar />
+      <button className={styles.addButton}>+</button>
+      <Home />
     </div>
   );
 }
---
+++ Front/src/Components/Admin/UploadImage/UploadImage.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { storage, firebaseApp } from "../Firebase/credentials";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { v4 } from "uuid";
+import NavBar from "../NavBar/NavBar";
+/* import styles from "./Admin.module.css"; */
+
+export default function UploadImage() {
+  const firestore = getFirestore(firebaseApp);
+
+  const [imageUpload, setImageUpload] = useState(null);
+
+  async function createInFirestore(uid, url) {
+    const docuRef = doc(firestore, `Cards/${uid}`);
+
+    setDoc(docuRef, {
+      Description: "uploaded",
+      ImgUrl: url,
+      Price: 1278,
+    });
+  }
+
+  function uploadImage() {
+    if (imageUpload === null) return;
+    const name = v4();
+    const imageRef = ref(storage, `images/${name}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      getDownloadURL(imageRef)
+        .then((url) => {
+          createInFirestore(name, url).then(() => alert("Image uploaded"));
+        })
+        .catch((error) => {
+          // Manejar errores si es necesario
+          console.error("Error al obtener la URL de descarga:", error);
+        });
+    });
+  }
+
+  return (
+    <div /* className={styles.adminContainer} */>
+      <NavBar />
+      <input
+        type="file"
+        onChange={(event) => {
+          setImageUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadImage}>Upload</button>
+    </div>
+  );
+}
